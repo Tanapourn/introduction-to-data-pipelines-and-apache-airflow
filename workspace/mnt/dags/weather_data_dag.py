cf4391632fc5968bad185fcae93e7a43081793b8
Tanapourn <puipuffypui@hotmail.com>
2026-06-20 09:05:26 +0000

    Try analytics engineering with duckdb

--- workspace/mnt/dags/weather_data_dag.py
+++ workspace/mnt/dags/weather_data_dag.py
@@ -38,6 +38,78 @@ def _find_average_pm2_5(**context):
         }
         json.dump(data, f, ensure_ascii=False, indent=2)
 
+def _check_pm2_5_key_exists (**context):
+    ds = context["ds"]
+    
+    import json
+
+    with open(f"/opt/airflow/dags/{ds}-output.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    
+    assert "hourly" in data #เช็คว่า key hourly เก็บไว้จริงใน data
+    assert "pm2_5" in data["hourly"] #เช็คว่า pm 2.5 อยู่ใน data hourly
+
+def _calculate_data_quality_score(**context):
+    import json
+    import glob
+    import os
+
+    pattern = os.path.join("/opt/airflow/dags", "*-output.json")
+    files = sorted(glob.glob(pattern))
+    results = []
+    count_ok = 0
+    for f in files:
+        with open(f, "r", encoding="utf-8") as fh:
+            data = json.load(fh)
+        pm = data["hourly"]["pm2_5"]
+        pm_count = len(pm)
+        if pm_count == 24:
+            count_ok += 1
+ 
+    total_files = len(files)
+    score = (count_ok / total_files)
+    print(f"Data Quality Score: {score}") 
+
+def _transform_to_csv(**context):
+    import csv
+    import json
+    import glob
+    import os
+
+    pattern = os.path.join("/opt/airflow/dags", "*-output.json")
+    files = sorted(glob.glob(pattern))
+    results = []
+    count_ok = 0
+    with open("/opt/airflow/dags/pm2_5_data.csv", "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["date", "hour", "pm2_5"])
+        for f in files:
+            with open(f, "r", encoding="utf-8") as fh:
+                data = json.load(fh)        
+                for i, pm_value in enumerate(data["hourly"]["pm2_5"]):
+                    writer.writerow([data["hourly"]["time"][0].split("T")[0], i, pm_value])
+
+def _find_average_pm2_5_in_duckdb(**context):
+    import duckdb
+    import os
+
+    conn = duckdb.connect(database="/opt/airflow/dags/myduckdb.db")
+    csv_file_path = "/opt/airflow/dags/pm2_5_data.csv"
+    conn.execute(f"""
+        CREATE TABLE IF NOT EXISTS pm2_5_data AS 
+        SELECT * FROM read_csv_auto('{csv_file_path}')
+    """)
+
+    query = """
+        CREATE OR REPLACE VIEW avg_pm2_5 as
+        select
+            date,
+            avg(pm2_5)
+        from pm2_5_data
+        group by date
+    """
+    conn.execute(query)
+
 with DAG( 
     "weather_data_dag", 
     start_date= timezone.datetime(2026, 6, 6), 
@@ -48,10 +120,33 @@ with DAG(
         task_id="get_air_quality_data",
         python_callable=_get_air_quality_data,
     )
+#check key เพื่อให้แน่ใจว่า calculate score ได้ 
+    check_pm2_5_key_exists = PythonOperator(
+        task_id="check_pm2_5_key_exists",
+        python_callable=_check_pm2_5_key_exists,
+    )
+
+    calculate_data_quality_score = PythonOperator(
+        task_id="calculate_data_quality_score",
+        python_callable=_calculate_data_quality_score,
+    )
 
     find_average_pm2_5 = PythonOperator(
         task_id="find_average_pm2_5",
         python_callable=_find_average_pm2_5,
     )
-    
-    get_air_quality_data >> find_average_pm2_5
+
+    transform_to_csv = PythonOperator(
+        task_id="transform_to_csv",
+        python_callable=_transform_to_csv,
+    )
+
+    find_average_pm2_5_in_duckdb = PythonOperator(
+        task_id="find_average_pm2_5_in_duckdb",
+        python_callable=_find_average_pm2_5_in_duckdb,
+    )
+
+#งานที่ operate จากคำสั่งข้างบน
+    get_air_quality_data >> check_pm2_5_key_exists >> find_average_pm2_5
+    check_pm2_5_key_exists >> calculate_data_quality_score
+    check_pm2_5_key_exists >> transform_to_csv >> find_average_pm2_5_in_duckdb 
